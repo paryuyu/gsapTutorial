2470ff0732493042f9eab87313436be4f9e8d1f0
yuyuyu <feelvu@alt-a.net>
2024-11-02 09:34:26 +0900

    init

---
+++ script.js
@@ -0,0 +1,21 @@
+gsap.to("#box1", {
+    x: 1000,
+    dration: 2,
+    delay: 1,
+    rotate: 360,
+})
+
+gsap.from("#box2", {
+    x: 1000,
+    dration: 2,
+    delay: 1,
+    rotate: 360,
+
+})
+
+// gsap.from("#box2", {
+//     x: 500,
+//     y: 500,
+//     dration: 2,
+//     delay: 1
+// })
