468cf9417fc6fd2edc642516988dec4d66b8ed18
yuyuyu <feelvu@alt-a.net>
2024-11-02 12:52:26 +0900

    gsap svg

---
+++ tutorial3/script.js
@@ -0,0 +1,22 @@
+let path = `M 10 100 Q 500 100 990 100`;
+let finalPath = `M 10 100 Q 500 100 990 100`;
+
+let string = document.querySelector('#string');
+
+string.addEventListener("mousemove", (dets) => {
+    path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`;
+    gsap.to("svg path", {
+        attr: { d: path },
+        duration: 0.3,
+        ease: "power3.out"
+            //https://gsap.com/docs/v3/Eases
+    })
+})
+
+string.addEventListener("mouseleave", () => {
+    gsap.to("svg path", {
+        attr: { d: finalPath },
+        duration: 1.5,
+        ease: "elastic.out(1,0.3)"
+    })
+})
